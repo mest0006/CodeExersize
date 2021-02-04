a4b250315abe41adef114b6e63b1f72cd6a03ab2
Borbala Mester <46438846+mest0006@users.noreply.github.com>
2021-02-04 18:35:28 -0500

    adding allfiles

---
+++ PHP/functions.php
@@ -0,0 +1,55 @@
+<?php
+
+########################
+###  EDIT THIS FILE  ###
+########################
+
+
+// read from "input.json" and return as an array
+function load_input_file_into_php_array()
+{
+
+  $strJsonFileContents = file_get_contents("data/input.json");
+  $input_array = json_decode($strJsonFileContents, true);
+
+  return $input_array;
+
+  print "Loading...\n";
+}
+
+
+// convert array to match structure in "correct-output.json"
+function convert_array_to_output_format($input_array)
+{
+  $input_array = load_input_file_into_php_array();
+  $arr = json_encode($input_array);
+  $newKeys  = array('birds' => null, 'bird' => null, 'EnglishName' => 'name', 'Species' => 'latin', 'Lifespan' => 'lifespan');
+
+  $tmp_arr =  array_map(function ($k) {
+    return '/\b' . $k . '\b/u';
+  }, array_keys($newKeys));
+
+  $output_array =  preg_replace($tmp_arr, array_values($newKeys), $arr);
+  return $output_array;
+  print "Converting...\n";
+}
+// save the array to file named "my-output.json" 
+function save_php_array_to_output_file($output_array)
+{
+  $inp = file_get_contents('my_output.json');
+  $output = convert_array_to_output_format($output_array);
+  $out = file_put_contents($output, $inp);
+  return $out;
+  print "Saving...\n";
+}
+
+
+########################################################################
+###  Note: Final "my-output.json" file should match structure of     ###
+###  "correct-output.json" - but, whitespace does NOT have to match. ###
+########################################################################
+
+########################################################################
+###  Tip - Look at these built-in PHP functions:                     ###
+###  json_encode, json_decode, c, file_get_contents  ###
+########################################################################
---
+++ README.txt
@@ -0,0 +1,15 @@
+READ ME
+=======
+
+This folder contains two exercises. 
+- HTML-CSS
+- PHP
+
+View the read me file in each folder for details.
+- README-HTML-CSS.txt
+- README-PHP.txt
+
+You have til the agreed upon time to complete both exercises.
+
+Good luck!
+
